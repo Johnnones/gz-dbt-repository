8ebdbc8712dce26229d2de94970ebf411d894623
John Nones <cerjohnnones@gmail.com>
2025-05-31 06:00:04 +0000

    operational margin computation

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,13 @@
+WITH order_margin AS(
+    SELECT *
+    FROM {{ref("int_orders_margin")}}
+)
+, ship AS(
+    SELECT *
+    FROM {{ref("stg_raw__ship")}}
+)
+SELECT order_margin.orders_id
+, order_margin.date_date
+, ROUND(order_margin.margin + ship.shipping_fee - ship.log_cost - ship.ship_cost,2) AS operational_margin
+FROM order_margin
+JOIN ship USING(orders_id)
